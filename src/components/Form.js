a42f7ef8e66fe722f9eb90d0fd1ff1fe00e6139b
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-01 11:58:17 +0530

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,27 +1,23 @@
-// Form.js
 import React from 'react';
 
-function Form() {
-    return (
-        <div>
-            <h1>Form Layout</h1>
-            <form id="info-form">
-                <label htmlFor="full_name">Full Name:</label>
-                <input type="text" id="full_name" name="full_name" />
-                <br />
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" />
-                <br />
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" />
-                <br />
-                <label htmlFor="password_confirmation">Confirm Password:</label>
-                <input type="password" id="password_confirmation" name="password_confirmation" />
-                <br />
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    );
-}
+const Form = () => {
+  return (
+    <form id="info-form">
+      <label htmlFor="full_name">Full Name:</label>
+      <input type="text" id="full_name" />
+
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" />
+
+      <label htmlFor="password">Password:</label>
+      <input type="password" id="password" />
+
+      <label htmlFor="password_confirmation">Confirm Password:</label>
+      <input type="password" id="password_confirmation" />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 export default Form;
