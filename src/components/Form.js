5b8a347b9a73270900decea31ce93ce00af90e3e
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-01 12:28:31 +0530

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,24 +2,26 @@ import React from 'react';
 
 const Form = () => {
   return (
-    <div id="info-form">
+    <form id="info-form">
       <h2>Form Layout</h2>
-      <form>
+      <div>
         <label htmlFor="full_name">Full Name:</label>
         <input type="text" id="full_name" />
-        <br />
+      </div>
+      <div>
         <label htmlFor="email">Email:</label>
         <input type="email" id="email" />
-        <br />
+      </div>
+      <div>
         <label htmlFor="password">Password:</label>
         <input type="password" id="password" />
-        <br />
+      </div>
+      <div>
         <label htmlFor="password_confirmation">Confirm Password:</label>
         <input type="password" id="password_confirmation" />
-        <br />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
   );
 };
 
