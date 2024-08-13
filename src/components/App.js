dfcca61cfddc1d6df24bb0c02177bfb67840a6e8
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-13 13:26:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,20 @@
-import React from 'react'
-import {  Link, Route, Routes } from 'react-router-dom'
-import Form from './Form'
-import FormRef from './FormRef'
-import FormState from './FormState'
- 
- 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 const App = () => {
   return (
-    <div>
-      <Link to='/form' id='form-link' >section 1</Link><br/>
-      <Link to='/form-ref' id='form-ref-link' >section 2</Link><br/>
-      <Link to='/form-state' id='form-state-link' >section 3</Link><br/>
-         <Routes>
-            <Route path='/form' element={<Form />} />
-            <Route path='/form-ref' element={ <FormRef />} />
-            <Route path='/form-state' element={ <FormState />} />
-         </Routes>
-    </div>
-  )
-}
- 
+    <Router>
+      <div>
+        <Link to='/form' id='form-link'>section 1</Link><br/>
+        <Link to='/form-ref' id='form-ref-link'>section 2</Link><br/>
+        <Link to='/form-state' id='form-state-link'>section 3</Link><br/>
+        <Routes>
+          <Route path='/form' element={<Form />} />
+          <Route path='/form-ref' element={<FormRef />} />
+          <Route path='/form-state' element={<FormState />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
 export default App;
