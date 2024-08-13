f96dbd167688c30e58a037a558d237b784ce45db
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-13 12:40:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,65 +1,23 @@
-import React, { useState, useEffect, useRef } from 'react';
-import Form from './Form';
-import FormRef from './FormRef';
-import FormState from './FormState';
-import Card from './Card';
-
+import React from 'react'
+import {  Link, Route, Routes } from 'react-router-dom'
+import Form from './Form'
+import FormRef from './FormRef'
+import FormState from './FormState'
+ 
+ 
 const App = () => {
-  const [activeSection, setActiveSection] = useState('form');
-  const formRef = useRef(null);
-
-  useEffect(() => {
-    const path = window.location.pathname.replace('/', '');
-    if (path) {
-      setActiveSection(path);
-    }
-  }, []);
-
-  useEffect(() => {
-    window.history.replaceState(null, '', `/${activeSection}`);
-  }, [activeSection]);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (formRef.current) {
-      formRef.current.handleSubmit();
-    }
-  };
-
-  const renderFormSection = () => {
-    switch (activeSection) {
-      case 'form':
-        return <Form ref={formRef} />;
-      case 'form-ref':
-        return <FormRef ref={formRef} />;
-      case 'form-state':
-        return <FormState ref={formRef} />;
-      default:
-        return null; // return null when activeSection doesn't match any form section
-    }
-  };
-
   return (
     <div>
-      <nav>
-        <ul>
-          <li>
-            <button onClick={() => setActiveSection('form')} id="form-link">Form Layout</button>
-          </li>
-          <li>
-            <button onClick={() => setActiveSection('form-ref')} id="form-ref-link">Form with useRef</button>
-          </li>
-          <li>
-            <button onClick={() => setActiveSection('form-state')} id="form-state-link">Form with useState</button>
-          </li>
-        </ul>
-      </nav>
-      <Card key={activeSection}>
-        {renderFormSection()}
-      </Card>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
+      <Link to='/form' id='form-link' >section 1</Link><br/>
+      <Link to='/form-ref' id='form-ref-link' >section 2</Link><br/>
+      <Link to='/form-state' id='form-state-link' >section 3</Link><br/>
+         <Routes>
+            <Route path='/form' element={<Form />} />
+            <Route path='/form-ref' element={ <FormRef />} />
+            <Route path='/form-state' element={ <FormState />} />
+         </Routes>
     </div>
-  );
-};
-
-export default App;
+  )
+}
+ 
+export default App
