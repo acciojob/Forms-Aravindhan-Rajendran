83b53990fe1f836daaa3e1dc0a7794a4acc1c2c9
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-13 12:39:52 +0530

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,37 +1,12 @@
-import React, { forwardRef, useImperativeHandle } from 'react';
-
-const Form = forwardRef((props, ref) => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log('Form Layout Submitted');
-    // Additional submit logic here
-  };
-
-  useImperativeHandle(ref, () => ({
-    handleSubmit
-  }));
-
+import React, { useRef } from 'react'
+import Card from './Card';
+ 
+const Form = () => {
   return (
-    <form id="info-form">
-      <h2>Form Layout</h2>
-      <div>
-        <label htmlFor="full_name">Full Name:</label>
-        <input type="text" id="full_name" />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" />
-      </div>
-      <div>
-        <label htmlFor="password_confirmation">Confirm Password:</label>
-        <input type="password" id="password_confirmation" />
-      </div>
-    </form>
-  );
-});
-
-export default Form;
+    <div>
+       <Card />
+    </div>
+  )
+}
+ 
+export default Form
