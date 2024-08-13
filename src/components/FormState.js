529596e8d192f869b97765bfff721d5edb65ee94
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-13 12:38:53 +0530

    Update FormState.js

--- src/components/FormState.js
+++ src/components/FormState.js
@@ -1,64 +1,21 @@
-import React, { forwardRef, useImperativeHandle, useState } from 'react';
-
-const FormState = forwardRef((props, ref) => {
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordConfirmation, setPasswordConfirmation] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log('Full Name:', fullName);
-    console.log('Email:', email);
-    console.log('Password:', password);
-    console.log('Password Confirmation:', passwordConfirmation);
-  };
-
-  useImperativeHandle(ref, () => ({
-    handleSubmit
-  }));
-
+import React, { useState } from 'react'
+import Card from './Card'
+ 
+const FormState = () => {
+    const [submitstate,setsubmitstate]=useState('')
+    const submit=()=>{
+        setsubmitstate(submitstate=>{
+         const val=  'Form submitted by useState';
+         console.log(val)
+         return val;
+        });
+    }
   return (
-    <form id="info-form" onSubmit={handleSubmit}>
-      <h2>Form with useState</h2>
-      <div>
-        <label htmlFor="full_name">Full Name:</label>
-        <input
-          type="text"
-          id="full_name"
-          value={fullName}
-          onChange={(e) => setFullName(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="password_confirmation">Confirm Password:</label>
-        <input
-          type="password"
-          id="password_confirmation"
-          value={passwordConfirmation}
-          onChange={(e) => setPasswordConfirmation(e.target.value)}
-        />
-      </div>
-    </form>
-  );
-});
-
+    <div>
+        <Card submit={submit} />
+        {submitstate}
+    </div>
+  )
+}
+ 
 export default FormState;
